83c08c5d1c3852cd2f71ebd1d16f7541ef12ff5c
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-01-08 20:29:30 +0100

    first breaktrough with config parsing

--- lib/blazing/cli.rb
+++ lib/blazing/cli.rb
@@ -2,31 +2,43 @@ require 'blazing/config'
 
 class Blazing::CLI < Thor
 
-  desc 'setup TARGET', 'bootstrap blazing setup and do a first deploy'
-  def setup(stage = 'default')
-    
-    # First time setup ?
-    if File.exists?('deploy/blazing.rb')
-      if yes? 'do you want to overwrite your current blazing configuration?', 'red'
-        say 'overwriting existing configuration'
-        install_from_scratch = true
-      end
-    else
-      install_from_scratch = true
-    end
+  #TODO: optional project directory
+  desc 'init', 'initialize blazing in project'
+  def init
+    username = ask "Deploy User: "
+    hostname = ask "Target hostname: "
+    path = ask "Deploy path on server: "
+    invoke 'blazing:config:create', [username, hostname, path]
+  end
 
-    if install_from_scratch
-      say 'bootstraping configuration'
-      Blazing::Config.create
+
+  desc 'setup TARGET', 'bootstrap or update blazing setup on remote host and do a first deploy'
+  def setup(target = nil)
+  
+    # Load config file
+    config = Blazing::Config.read do |blazing|
+      blazing.instance_eval(File.read blazing.file)
     end
+    p config.targets
+
+    #if target.nil?
+
+      # check if there is a default target
+      # - by checking if there is a target in global
+      # - or there is a default_target in global 
+      # => assign if present, else ask:
+      
+    # target = target || ask("Deployment Target: ") 
+
+    #Blazing::Remote.clone
+    #Blazing::Remote.add_pre_receive_hook
+    #Blazing::Remote.add_post_receive_hook
     
-    # add pre-receive hook
-    # add post-receive hook
   end
 
   desc 'deploy TARGET', 'deploy project with blazing'
   def deploy
-    # check if deployed setup is still uptodate, if not, run setup
+    # TODO: check if deployed setup is still uptodate, if not, run setup
     # tag and push
   end
 
--- lib/blazing/config.rb
+++ lib/blazing/config.rb
@@ -1,7 +1,52 @@
+require 'thor/group'
+
 class Blazing::Config
 
-  def create
-    puts 'helloo'
+  attr_accessor :targets, :file
+
+  def initialize
+    @targets = []
+    @file = File.open('deploy/blazing.rb')
+  end
+
+  def self.read(&block)
+    config = Blazing::Config.new
+
+    p config
+
+    block.arity < 1 ? config.instance_eval(&block) : block.call(config)
+    
+
+    p config
+
+    return config
+  end
+
+  def target(name)
+    @targets << name
+    p "asdasd #{@targets}"
+  end
+
+  class Create < Thor::Group
+
+    include Thor::Actions
+
+    argument :username
+    argument :hostname
+    argument :path
+
+    def self.source_root
+      File.dirname(__FILE__)
+    end
+
+    def create_blazing_dir
+      empty_directory 'deploy'
+    end
+
+    def create_config_file
+      template('templates/blazing.tt', "deploy/blazing.rb")
+    end
+
   end
 
 end
