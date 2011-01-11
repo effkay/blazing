a1d0660a34bca602155f4f9ac2a1b0fe2a2bdc5f
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-01-11 23:01:32 +0100

    stubbed deployment methods

--- lib/blazing/remote.rb
+++ lib/blazing/remote.rb
@@ -1,8 +1,14 @@
 #
 # Stuff that runs on remote machine
 #
-class Blazing::Remote < Thor
+class Blazing::Remote
 
-  
+  def pre_receive
+    
+  end
+
+  def post_receive
+    `rebase --hard HEAD`
+  end
 
 end
