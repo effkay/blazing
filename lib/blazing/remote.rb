782e4e56aa468406818be2946f441c116a0c16cd
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-01-17 17:49:45 +0100

    writing some todos and ideas

--- lib/blazing/remote.rb
+++ lib/blazing/remote.rb
@@ -4,11 +4,16 @@
 class Blazing::Remote
 
   def self.pre_receive(target_name)
-    
+    # really needed? probably only for maintenance page, maybe even this can be done post receive
   end
 
   def self.post_receive(target_name)
+    
+    target = Blazing::Config::Helper.find_target(target_name)
+
     `git reset --hard HEAD`
+
+    `bundle update`
   end
 
 end
