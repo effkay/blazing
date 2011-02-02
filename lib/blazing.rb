28033de6dca56d41a7d7c873aa2afbf72dcae5ad
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-02-02 19:39:14 +0100

    change load order

--- lib/blazing.rb
+++ lib/blazing.rb
@@ -1,14 +1,13 @@
 require 'thor'
 require 'thor/group'
 require 'blazing'
-require 'blazing/cli/base'
-
 require 'blazing/config'
 require 'blazing/logger'
 require 'blazing/target'
 require 'blazing/remote'
 require 'blazing/recipe'
 require 'blazing/object'
+require 'blazing/cli/base'
 require 'blazing/cli/create'
 require 'blazing/cli/hook'
 
