2f4a063321267de9a5b22fa64c0e5b641bfba610
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-05-19 23:03:04 +0200

    specs for config generator

--- spec/blazing/cli/create_spec.rb
+++ spec/blazing/cli/create_spec.rb
@@ -1,6 +1,25 @@
 require 'spec_helper'
+require 'blazing'
 require 'blazing/cli/create'
 
 describe Blazing::CLI::Create do
-  pending 'write cli/create specs'
+
+  before :each do
+    @config_generator = Blazing::CLI::Create.new
+  end
+
+  it 'knows the source root for its tempate' do
+    Blazing::CLI::Create.source_root.should == File.dirname(__FILE__).gsub('spec', 'lib')
+  end
+
+  it 'creates an empty directory for the config file' do
+    @config_generator.should_receive(:empty_directory).with(Blazing::DIRECTORY)
+    @config_generator.create_blazing_dir
+  end
+
+  it 'creates the config file' do
+    @config_generator.should_receive(:template).with('templates/blazing.tt', Blazing::CONFIGURATION_FILE)
+    @config_generator.create_config_file
+  end
+
 end
