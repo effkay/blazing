b534c919ef4c05efb447feedbd4a660e291c6bd6
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-06-06 07:51:55 +0200

    rvm recipe spec

--- lib/blazing/recipes/rvm_recipe.rb
+++ lib/blazing/recipes/rvm_recipe.rb
@@ -4,25 +4,7 @@ module Blazing
   class RvmRecipe < Blazing::Recipe
 
     def run
-      if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
-        begin
-          rvm_path     = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
-          rvm_lib_path = File.join(rvm_path, 'lib')
-          $LOAD_PATH.unshift rvm_lib_path
-          require 'rvm'
-          RVM.use @options[:rvm_string]
-        rescue LoadError
-          raise "RVM ruby lib is currently unavailable."
-        end
-      end
-
-      # TODO: ADD LOGGER!
-      if $?.exitstatus == 0
-        puts 'successfully executed rvm switch trough recipe'
-      else
-        puts 'ohno!'
-      end
-
+      false
     end
 
   end
--- spec/blazing/recipes/rvm_recipe_spec.rb
+++ spec/blazing/recipes/rvm_recipe_spec.rb
@@ -2,5 +2,9 @@ require 'spec_helper'
 require 'blazing/recipes/rvm_recipe'
 
 describe Blazing::RvmRecipe do
-  pending 'write rvm recipe specs'
+  describe '#run' do
+    it 'returns false, as the recipe itself is handled in shellscript' do
+      Blazing::RvmRecipe.new('rvm_recipe').run.should be false
+    end
+  end
 end
