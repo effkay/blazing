72505e47996b79b94ac5a28d170524f7a9e6b10d
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-10-20 18:49:03 +0200

    move spec helper, add caputre helper

---
+++ spec/support/spec_helper.rb
@@ -0,0 +1,21 @@
+#
+# Stuff borrowed from carlhuda/bundler
+#
+RSpec.configure do |config|
+  def capture(stream)
+    begin
+      stream = stream.to_s
+      eval "$#{stream} = StringIO.new"
+      yield
+      result = eval("$#{stream}").string
+    ensure
+      eval("$#{stream} = #{stream.upcase}")
+    end
+
+    result
+  end
+
+  def destination_root
+    File.join(File.dirname(__FILE__), 'sandbox')
+  end
+end
