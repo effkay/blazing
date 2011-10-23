e09c10f0810e55272385655dbdb270577ba717ce
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-10-24 01:27:30 +0200

    remove obsolete specs

--- spec/blazing/runner_spec.rb
+++ spec/blazing/runner_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 require 'blazing/runner'
+require 'blazing/config'
 
 #
-# Note: More specs for Runner in the integration directory.
+# NOTE: More specs for Runner in the integration directory.
 #
 
 describe Blazing::Runner do
@@ -16,30 +17,6 @@ describe Blazing::Runner do
 
     it 'fails if the command does not exist' do
       lambda { Blazing::Runner.new(@config).exec('weird_command') }.should raise_error
-    end
-
-    it 'knows the init command' do
-      runner = Blazing::Runner.new(@config)
-      runner.should_receive(:init_command)
-      runner.exec('init')
-    end
-
-    it 'knows the setup:local command' do
-      runner = Blazing::Runner.new(@config)
-      runner.should_receive(:setup_local_command)
-      runner.exec('setup:local')
-    end
-
-    it 'knows the setup:remote command' do
-      runner = Blazing::Runner.new(@config)
-      runner.should_receive(:setup_remote_command)
-      runner.exec('setup:remote')
-    end
-
-    it 'knows the deploy command' do
-      runner = Blazing::Runner.new(@config)
-      runner.should_receive(:deploy_command)
-      runner.exec('deploy')
     end
 
   end
