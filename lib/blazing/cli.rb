cf404d5a52d2bb0c2da0156ca3fec6f4f7b5325b
Felipe Kaufmann <felipekaufmann@gmail.com>
2012-01-20 23:22:48 +0100

    reorder methods

--- lib/blazing/cli.rb
+++ lib/blazing/cli.rb
@@ -7,36 +7,56 @@ module Blazing
     default_task :help
 
     desc 'init', 'Generate a blazing config file'
+
+    #
+    # Bootstrap blazing by creating a sample config file
+    #
     def init
       Blazing::Runner.init
     end
 
+    desc 'setup [TARGET]', 'Setup local and remote repository/repositories for deployment'
+
     method_option :file,
       :type    => :string,
       :aliases => '-f',
       :banner  => 'Specify a configuration file'
 
-    desc 'setup [TARGET]', 'Setup local and remote repository/repositories for deployment'
+    #
+    # Setup a target to be deployed with blazing
+    #
     def setup(target_name = nil)
       Blazing::Runner.setup(target_name, options)
     end
 
+    desc 'update [TARGET]', 'Re-Generate and uplaod hook based on current configuration'
+
     method_option :file,
       :type    => :string,
       :aliases => '-f',
       :banner  => 'Specify a configuration file'
 
-    desc 'update [TARGET]', 'Re-Generate and uplaod hook based on current configuration'
+    #
+    # Update the target hook so it matches the settings in the config
+    #
     def update(target_name = nil)
       Blazing::Runner.update(target_name, options)
     end
 
-    desc 'recipes', 'Run the Recipes (used on remote machine)'
+    desc 'recipes', 'Run the recipes'
+
+    #
+    # Run the configured blazing recipes (used on remote machien)
+    #
     def recipes(target_name)
       Blazing::Runner.recipes(target_name, options)
     end
 
-    desc 'list', 'List available Recipes'
+    desc 'list', 'List available recipes'
+
+    #
+    # List the available blazing recipes
+    #
     def list
       Blazing::Runner.list
     end
