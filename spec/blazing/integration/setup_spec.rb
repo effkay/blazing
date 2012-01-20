eaade11ad26d160a2b6c41f3dcd0ad4d015e768d
Felipe Kaufmann <felipekaufmann@gmail.com>
2012-01-21 00:14:26 +0100

    add pending specs for update and setup

--- spec/blazing/integration/setup_spec.rb
+++ spec/blazing/integration/setup_spec.rb
@@ -1,32 +1,17 @@
-# require 'spec_helper'
-# require 'blazing/config'
-# require 'blazing/runner'
-# require 'grit'
+require 'spec_helper'
 
-# describe 'blazing setup' do
+  describe 'blazing setup' do
 
-#   before :each do
-#     setup_sandbox
-#     @production_url = 'user@host:/some/where'
-#     @staging_url = 'user@host:/some/where/else'
+    before :each do
+      setup_sandbox
+    end
 
-#     @config = Blazing::Config.new
-#     @config.target :production, @production_url
-#     @config.target :staging, @staging_url, :default => true
-#     @runner = Blazing::Runner
-#   end
+    after :each do
+      teardown_sandbox
+    end
 
-#   after :each do
-#     teardown_sandbox
-#   end
+   it 'prepares the repository on the target location'
+   it 'configures the repository to allow pushing to the checked out branch'
+   it 'runs blazing update for the target'
 
-#   it 'clones the repository, configures it and updates the target' do
-#     @shell = Blazing::Shell.new
-#     @target = @config.default_target
-#     @target.instance_variable_set('@shell', @shell)
-#     @shell.should_receive(:run).with("ssh user@host 'mkdir /some/where/else && cd /some/where/else && git init && cd /some/where/else && git config receive.denyCurrentBranch ignore'")
-#     @target.should_receive(:apply_hook)
-#     @runner.setup
-#   end
-
-# end
+ end
