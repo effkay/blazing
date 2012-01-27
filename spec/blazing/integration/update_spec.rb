293ffe286c14b11e51c038edac7a2eb3cb6323ed
Felipe Kaufmann <felipekaufmann@gmail.com>
2012-01-27 23:51:18 +0100

    implement update spec

--- spec/blazing/integration/update_spec.rb
+++ spec/blazing/integration/update_spec.rb
@@ -4,14 +4,24 @@ require 'spec_helper'
 
     before :each do
       setup_sandbox
+      @config = Blazing::Config.new
+      @config.target :production, "#{@sandbox_directory}/target"
+      @cli = Blazing::CLI.new
+      Blazing::Config.stub(:parse).and_return @config
+      @output = capture(:stdout, :stderr) { @cli.setup(:production) }
     end
 
     after :each do
       teardown_sandbox
     end
 
-   it 'generates a post-receive hook based on the current blazing config'
-   it 'copies the generated post-receive hook to the target'
+   it 'generates a post-receive hook based on the current blazing config' do
+     File.exists?("/tmp/post-receive").should be true
+   end
+
+   it 'copies the generated post-receive hook to the target' do
+     File.exists?("#{@sandbox_directory}/target/.git/hooks/post-receive").should be true
+   end
 
  end
 
