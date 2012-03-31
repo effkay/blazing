ee0c03d709c5ceb95dbf06198bbecb829174673c
Felipe Kaufmann <felipekaufmann@gmail.com>
2012-03-31 23:45:36 +0200

    add cli spec

---
+++ spec/blazing/cli_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+module Blazing
+  describe CLI do
+
+    let(:cli) { CLI.new }
+    it 'has an init method' do
+      cli.respond_to? :init
+    end
+
+    it 'has a setup method' do
+      cli.respond_to? :setup
+    end
+
+    it 'has an update method' do
+      cli.respond_to? :update
+    end
+
+    it 'has a recipes method' do
+      cli.respond_to? :recipes
+    end
+
+    it 'has a list method' do
+      cli.respond_to? :list
+    end
+  end
+end
+
