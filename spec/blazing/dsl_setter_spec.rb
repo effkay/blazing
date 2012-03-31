96205b5a0e688731153677086b5779dd7201ef38
Felipe Kaufmann <felipekaufmann@gmail.com>
2012-03-31 23:58:35 +0200

    add dsl setter spec

---
+++ spec/blazing/dsl_setter_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+module Blazing
+  describe DSLSetter do
+
+    let(:dummy) { class Dummy; extend DSLSetter; end }
+    let(:dummy_instance) { Dummy.new }
+
+    it 'it defines a dsl method named after each argument provided' do
+      dummy.dsl_setter(:foo, :bar)
+      dummy_instance.should respond_to :foo
+      dummy_instance.should respond_to :bar
+    end
+
+    context 'the generated dsl method' do
+      it 'sets an instance variable when provided with an argumnent' do
+        dummy_instance.foo('something')
+        dummy_instance.instance_variable_get(:@foo).should == 'something'
+      end
+
+      it 'returns the value of the istance variable when no argument is provided' do
+        dummy_instance.instance_variable_set(:@foo, 'something')
+        dummy_instance.foo.should == 'something'
+      end
+    end
+  end
+end
+
