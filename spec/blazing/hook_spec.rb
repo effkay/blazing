e7813c6e6f7eacfc6dc7b4aedca84fdfcf95b33b
Felipe Kaufmann <felipekaufmann@gmail.com>
2012-04-01 12:56:15 +0200

    reimplement rake_command specs for (#64)

--- spec/blazing/hook_spec.rb
+++ spec/blazing/hook_spec.rb
@@ -4,17 +4,27 @@ module Blazing
 
   describe Hook do
 
+    let(:config) { Config.new }
+
     describe '#rake_command' do
       it 'prepends the environment variables specified in the rake call' do
-        pending
+        config.rake :deploy, 'SOMEFUNKYVARIABLE=foobar'
+        target = Blazing::Target.new(:sometarget, '/path', config)
+        hook = Hook.new(target)
+        hook.rake_command.should == 'SOMEFUNKYVARIABLE=foobar  bundle exec rake deploy'
       end
 
       it 'appends the RAILS_ENV specified as :rails_env option to the target call' do
-        pending
+        config.rake :deploy
+        target = Blazing::Target.new(:sometarget, '/path', config, :rails_env => 'production')
+        hook = Hook.new(target)
+        hook.rake_command.should == ' RAILS_ENV=production bundle exec rake deploy'
       end
 
       it 'returns nil when no rake task was specified in config' do
-        pending
+        target = Blazing::Target.new(:sometarget, '/path', config, :rails_env => 'production')
+        hook = Hook.new(target)
+        hook.rake_command.should be nil
       end
     end
   end
